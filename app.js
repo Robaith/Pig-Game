6e058a0d4d63b56b8ecf83c066dd8d2397382d23
Robaith <56103418+Robaith@users.noreply.github.com>
2020-06-14 21:18:22 +0600

    Update app.js

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
         scores[activePlayer] += roundScore;
         document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
 
-        if (scores[activePlayer] >= 10){
+        if (scores[activePlayer] >= 100){
             document.querySelector('#name-' + activePlayer).textContent = 'Winner';
             document.querySelector('.dice').style.display = 'none';
             document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
